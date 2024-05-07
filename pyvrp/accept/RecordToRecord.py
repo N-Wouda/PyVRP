d541b5aa6ac82fc050e77938864024314741ece2
Leon Lan <l.lan@vu.nl>
2024-05-07 08:20:41 +0200

    Implement RecordToRecord

--- pyvrp/accept/RecordToRecord.py
+++ pyvrp/accept/RecordToRecord.py
@@ -1,3 +1,29 @@
+import time
+
+
 class RecordToRecord:
-    # TODO
-    pass
+    def __init__(self, start_pct: float, end_pct: float, max_runtime: float):
+        self._start_pct = start_pct
+        self._end_pct = end_pct
+        self._max_runtime = max_runtime
+
+        self._delta_pct = start_pct - end_pct
+        self._start_time = time.perf_counter()
+
+    @property
+    def start_pct(self) -> float:
+        return self._start_pct
+
+    @property
+    def end_pct(self) -> float:
+        return self._end_pct
+
+    @property
+    def max_runtime(self) -> float:
+        return self._max_runtime
+
+    def __call__(self, best: float, curr: float, cand: float):
+        elapsed = (time.perf_counter() - self._start_time) / self.max_runtime
+        threshold = (self._delta_pct * (1 - elapsed) + self.end_pct) * best
+
+        return cand - best <= threshold
