5eb0e8f7a8b1e319ba2970fc01d203b6973290fe
Leon Lan <l.lan@vu.nl>
2024-05-07 08:14:48 +0200

    Wrapper for greedy repair

--- pyvrp/repair/greedy.py
+++ pyvrp/repair/greedy.py
@@ -1,3 +1,27 @@
-def greedy():
-    # TODO
-    pass
+import random as random
+
+from pyvrp._pyvrp import (
+    CostEvaluator,
+    ProblemData,
+    Solution,
+)
+from pyvrp.repair import greedy_repair
+
+
+def greedy(
+    data: ProblemData,
+    solution: Solution,
+    cost_eval: CostEvaluator,
+) -> Solution:
+    """
+    Small wrapper around ``greedy_repair`` to have Solution's as input and
+    output.
+    """
+    visited = {idx for route in solution.routes() for idx in route.visits()}
+    clients = range(data.num_depots, data.num_locations)
+    unplanned = [idx for idx in clients if idx not in visited]
+    random.shuffle(unplanned)
+
+    routes = greedy_repair(solution.routes(), unplanned, data, cost_eval)
+    not_empty = [route for route in routes if route.visits()]
+    return Solution(data, not_empty)
