2e915837baa485dc478279c59e6208963168a43f
Leon Lan <l.lan@vu.nl>
2024-05-20 14:47:12 +0200

    Add MovingAvergeThreshold

--- pyvrp/accept/__init__.py
+++ pyvrp/accept/__init__.py
@@ -1,2 +1,5 @@
 from .AcceptanceCriterion import AcceptanceCriterion as AcceptanceCriterion
+from .MovingAverageThreshold import (
+    MovingAverageThreshold as MovingAverageThreshold,
+)
 from .RecordToRecord import RecordToRecord as RecordToRecord
